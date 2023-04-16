5036e580cd6b625004e13bea63d54bdf11e029ed
vida <m.vidamohammadi@gmail.com>
2023-04-16 16:06:15 -0700

    add search box to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -53,15 +53,83 @@ class _HomePageState extends State<HomePage> {
 
           SizedBox(height: 25),
 
-          Container(
-            child: Row(children: [
-              Column(children: [
-                Text('How do you feel?'),
-                Text('Fill out your medical card right now'),
-              ],
-              )
-            ]),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.pink[100],
+                  borderRadius: BorderRadius.circular(12)
+              ),
+              child: Row(children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.deepPurple[200],
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                        ),
+                    ),
+                    SizedBox(height: 12,),
+                    Text(
+                        'Fill out your medical card right now',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                        ),
+                    ),
+                    SizedBox(height: 12,),
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[300],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text(
+                            'Get Started',
+                            style: TextStyle(color: Colors.white),
+                        ),
+
+                      ),
+                    ),
+                  ],
+                  ),
+                )
+              ]),
+            ),
           ),
+
+          SizedBox(height: 25),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(12)
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  hintText: 'How can we help you?',
+                ),
+              ),
+            ),
+          ),
+
+
         ],),
       )
     );
